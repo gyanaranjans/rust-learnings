4b0cf7a486e429fe6f3ceb85667a428582b2a94f
gyanaranjans <g44sahoo@gmail.com>
2024-06-24 09:09:47 +0530

    added nix-config

--- src/array.rs
+++ src/array.rs
@@ -1,15 +1,11 @@
 pub fn arry() {
     let arr = [1, 2, 3, 4, 5, 6, 7, 8];
-    let mut loop_idx = 0;
-    loop {
-        if arr[loop_idx] % 2 == 0 {
-            loop_idx += 1;
-            continue;
-        }
-        if arr[loop_idx] == 8 {
+    for &num in arr.iter() {
+        if num == 8 {
             break;
         }
-        println!("val : {}", arr[loop_idx]);
-        loop_idx += 1;
+        if num % 2 != 0 {
+            println!("val : {}", num);
+        }
     }
 }
