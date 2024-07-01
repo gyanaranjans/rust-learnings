0ed1bbf3041f2f84979ed2a432bda0b11a584bc4
gyanaranjans <g44sahoo@gmail.com>
2024-07-01 14:00:01 +0530

    added nix flake
    nix flake to install just rust

--- flake.nix
+++ flake.nix
@@ -7,9 +7,9 @@
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.rust;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.rust;
 
   };
 }
